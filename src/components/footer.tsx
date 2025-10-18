89320cc4a2864d36174e9f0b2df9c208e54ee61b
Alejandro <alejandro.uy.0@gmail.com>
2025-10-18 14:39:16 -0300

    about page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -46,7 +46,9 @@ const Footer = ({
         <div className="cm-footer-sep mt-7.5 mb-7.5 pt-7.5 flex items-center justify-between">
           <Link href="https://www.themoviedb.org/" target="_blank">
             <Image
-              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB"
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+              alt="Powered by TMDB"
+              title="Powered by TMDB"
               width={129} height={16}
               style={{ width: "129px", height: "16px" }}
               priority
