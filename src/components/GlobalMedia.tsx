d81769bfa75e659cd78a28bc0c77e5fbb1dbe06a
Alejandro <alejandro.uy.0@gmail.com>
2025-10-22 13:07:22 -0300

    style fix

--- src/components/GlobalMedia.tsx
+++ src/components/GlobalMedia.tsx
@@ -34,7 +34,7 @@ export function GlobalMedia({ media }: GlobalMediaProps) {
 
   if (media.backdrop_path) {
     return (
-      <div className='relative w-[622px] h-[350px] flex-shrink-0'>
+      <div className='relative w-auto h-auto lg:w-[622px] lg:h-[350px] flex-shrink-0'>
         <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center text-white text-2xl font-bold z-9'>
           Trailer not available
         </div>
